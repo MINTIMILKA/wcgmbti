7bb76a3301f1e7a1e2c3d7378f8f25fbd1879e8f
MINTIMILKA <57835814+MINTIMILKA@users.noreply.github.com>
2022-01-13 00:37:20 +0900

    Add files via upload

--- test/result.js
+++ test/result.js
@@ -382,69 +382,13 @@ Kakao.init('276e719d8448d0a0ea5ce688a4023792');
 //공유 기능
 function test_share()
 {
-  Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: '오늘의 디저트',
-      description: '아메리카노, 빵, 케익',
-      imageUrl:
-        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-      link: {
-        mobileWebUrl: 'http://www.gamebti.com',
-        androidExecutionParams: 'test',
-      },
-    },
-    itemContent: {
-      profileText: 'Kakao',
-      profileImageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-      titleImageUrl: "http://www.gamebti.com/4_result/8/21.png",
-      titleImageText: 'Cheese cake',
-      titleImageCategory: 'Cake',
-      items: [
-        {
-          item: 'Cake1',
-          itemOp: '1000원',
-        },
-        {
-          item: 'Cake2',
-          itemOp: '2000원',
-        },
-        {
-          item: 'Cake3',
-          itemOp: '3000원',
-        },
-        {
-          item: 'Cake4',
-          itemOp: '4000원',
-        },
-        {
-          item: 'Cake5',
-          itemOp: '5000원',
-        },
-      ],
-      sum: '총 결제금액',
-      sumOp: '15000원',
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
-    buttons: [
+    Kakao.Link.sendCustom
+    (
       {
-        title: '웹으로 이동',
-        link: {
-          mobileWebUrl: 'http://www.gamebti.com',
-        },
-      },
-      {
-        title: '앱으로 이동',
-        link: {
-          mobileWebUrl: 'http://www.gamebti.com',
-        },
-      },
-    ]
-  });
+        templateId:68513, 
+        imageUrl:'http://www.gamebti.com/4_result/8/20.png'
+      }
+    );
 }
 
 function loading(next_function)
